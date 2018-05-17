dec5cf988dc746e2447e2c1c5659db0c5974e5d7
Jetmir Haxhisefa <jettmir.h@gmail.com>
2018-05-17 16:56:48 +0200

    webmix

--- resources/assets/js/plugins-panels.js
+++ resources/assets/js/plugins-panels.js
@@ -1,3 +1,3 @@
-Vue.component('Accio_PostPositionManager_post', require('./../../../plugins/Accio/PostPositionManager/resources/views/panels/PostPanel.vue'));
-Vue.component('Accio_InstantArticles_post', require('./../../../plugins/Accio/InstantArticles/resources/views/panels/Post.vue'));
 Vue.component('Accio_SEO_post', require('./../../../plugins/Accio/SEO/resources/views/panels/Base.vue'));
+Vue.component('Accio_InstantArticles_post', require('./../../../plugins/Accio/InstantArticles/resources/views/panels/Post.vue'));
+Vue.component('Accio_PostPositionManager_post', require('./../../../plugins/Accio/PostPositionManager/resources/views/panels/PostPanel.vue'));
